b2a7198f4a395fb6b2823d3cfab9ad0e7c7771c3
Sid <rm.siddartha@gmail.com>
2024-07-02 12:18:33 -0500

    cmdlets sample code

---
+++ cmdlets/Cmdlets.ps1
@@ -0,0 +1,7 @@
+# <verb>-<action>
+
+Get-ChildItem # if we don't specify the path it will run in the current folder
+
+Get-ChildItem -Path c:\ # this will return everthing in the C drive
+
+Get-ChildItem -Path c:\ -Recurse # this will return all the childern of c drive folders
