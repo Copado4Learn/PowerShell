22db4073ac0c4764af6ffdc0f888e9714ac9bfbc
Sid <rm.siddartha@gmail.com>
2024-07-05 11:27:11 -0500

    added getcommand script and update gethlp

---
+++ Getcommand/getcommand.ps1
@@ -0,0 +1,3 @@
+# if we want to know the complete cmdlet, we can use get command along with wild card to pull the complete command
+
+Get-Command get-ev*
--- gethelp/gethelp.ps1
+++ gethelp/gethelp.ps1
@@ -4,3 +4,5 @@
 get-help Stop-Process -Online # this will take you to stop-process documention online
 
 get-help Stop-Process -ShowWindow # this will open up a window with description and examples on how to use stop-process cmdlet
+
+Update-Help # this command is used to update the modules in local system, also we can only update it once in 24 hours. To force update we need to add -force
