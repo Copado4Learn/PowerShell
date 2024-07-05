fbc45156acf435cf1d96b7d9982f30ba6ad5b313
Sid <rm.siddartha@gmail.com>
2024-07-05 10:05:08 -0500

    update gethelp script

--- gethelp/gethelp.ps1
+++ gethelp/gethelp.ps1
@@ -2,3 +2,5 @@
 #using get-help we can find necessary information about any cmdlet
 
 get-help Stop-Process -Online # this will take you to stop-process documention online
+
+get-help Stop-Process -ShowWindow # this will open up a window with description and examples on how to use stop-process cmdlet
